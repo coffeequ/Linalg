4e04f5fee9bb0ff75c6fc9aad675ca85e205e51b
coffeequ <savvapugach2004@mail.ru>
2026-08-24 13:24:46 +0500

    feat: refactor add and add more methods

--- internal/linalType/vector2D.go
+++ internal/linalType/vector2D.go
@@ -6,31 +6,48 @@ import (
 )
 
 type Vector2D struct {
-	Data [2]float64
+	data [2]float64
 }
 
 // func (имя_параметра тип_получателя) име_метода(параметры) (тип возвращаемого значения)
-func New(data [2]float64) *Vector2D {
-	return &Vector2D{Data: data}
+func NewVector2D(data [2]float64) *Vector2D {
+	return &Vector2D{data: data}
 }
 
-func (curr *Vector2D) Add(data [2]float64) {
-	curr.Data[0] += data[0]
-	curr.Data[1] += data[1]
+func (curr *Vector2D) X() float64 {
+	return curr.data[0]
 }
 
-func (curr Vector2D) Getter() [2]float64 {
-	return curr.Data
+func (curr *Vector2D) Y() float64 {
+	return curr.data[1]
 }
 
-func (curr Vector2D) Print() {
+func (curr *Vector2D) Add(antoher *Vector2D) (ptrRes *Vector2D) {
+	var temp [2]float64
+
+	temp[0] = curr.X() + antoher.X()
+
+	temp[1] = curr.Y() + antoher.Y()
+
+	ptrRes = &Vector2D{
+		data: temp,
+	}
+
+	return
+}
+
+func (curr *Vector2D) Getter() [2]float64 {
+	return curr.data
+}
+
+func (curr *Vector2D) Print() {
 	fmt.Println(curr.Getter())
 }
 
 func (curr Vector2D) GetLength() float64 {
-	return math.Sqrt(curr.Data[0]*curr.Data[0] + curr.Data[1]*curr.Data[1])
+	return math.Sqrt(curr.data[0]*curr.data[0] + curr.data[1]*curr.data[1])
 }
 
-func (curr *Vector2D) Dot(antoher [2]float64) float64 {
-	return curr.Data[0]*antoher[0] + curr.Data[1]*antoher[1]
+func (curr *Vector2D) Dot(antoher []float64) float64 {
+	return curr.data[0]*antoher[0] + curr.data[1]*antoher[1]
 }
