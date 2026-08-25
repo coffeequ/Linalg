6c0ccf8c67e0b3bd55a526d80f7b63b2fba508eb
coffeequ <savvapugach2004@mail.ru>
2026-08-25 20:44:16 +0500

    feat: test func

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	testData := [2]float64{1.2, 3.2}
+	testData := [2]float64{11, 12}
 
 	var testVector2D *linaltype.Vector2D = linaltype.NewVector2D(testData)
 
 	var anotherVector *linaltype.Vector2D = linaltype.NewVector2D([2]float64{0, 0})
 
 	fmt.Println(testVector2D.GetLength())
+
+	anotherVector.SetX(3)
+
+	anotherVector.SetY(2)
+
+	testVector2D.MultpValue(2)
+
+	testVector2D.Print()
+
+	testVector2D.DivisionValue(2)
+
+	testVector2D.Print()
 
 	var err = anotherVector.Normalize()
 
